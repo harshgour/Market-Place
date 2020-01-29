058b84db87712e6ae044437c0030afbd5d7cbaa2
Harsh Gour <harshgour57@gmail.com>
2020-01-29 21:53:36 +0530

    first

---
+++ JS.js
@@ -0,0 +1,8 @@
+<script>
+var slider = document.getElementById("myRange");
+var output = document.getElementById("price");
+output.innerHTML = slider.value;
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+</script>
